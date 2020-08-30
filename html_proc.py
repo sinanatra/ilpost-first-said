49d01be7af98d3a74e5e4ebe4daeb7c0f2529622
Giacomo Nanni <gn.nanni@gmail.com>
2020-08-30 18:09:47 +0200

    feat: first commit

---
+++ html_proc.py
@@ -0,0 +1,73 @@
+import os
+import glob
+import json
+import urllib.request
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+from datetime import datetime
+
+date = datetime.today().strftime('%Y-%m-%d')
+dateParser = datetime.today().strftime('%Y/%m/%d')
+
+# this is the main dictionary
+with open('./dictionary.json', 'r') as file:
+    data = file.read()
+
+dictionary = json.loads(data)
+
+#  save array to be visualised later
+database = open('./database/' + date + '.tsv', 'w')
+database.write('page' + '\t' + 'tokens' + '\t' + 'newtokens' + '\n')
+
+headers = {
+    'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:24.0) Gecko/20100101 Firefox/24.0'}
+
+req = urllib.request.Request("https://www.ilpost.it", None, headers)
+html = urllib.request.urlopen(req).read()
+soup = BeautifulSoup(html, features="lxml")
+
+for a_tag in soup.find_all('a', href=True):
+    print('href: ', a_tag['href'])
+    if( dateParser in a_tag['href']):
+        try:
+            innerHtml = urllib.request.urlopen(a_tag['href']).read()
+            innerSoup = BeautifulSoup(innerHtml, features="lxml")
+            
+            # ignore all scripts and css
+            for script in innerSoup(["script", "style"]):
+                script.extract()
+
+            # get  and cleans the text
+            text = innerSoup.get_text()
+            lines = (line.strip() for line in text.splitlines())
+            chunks = (phrase.strip()
+                      for line in lines for phrase in line.split("  "))
+            text = '\n'.join(chunk for chunk in chunks if chunk)
+
+            # get tokens - ignore punctuation and capital letters
+            tokenizer = RegexpTokenizer(r'\w+')
+            tokens = tokenizer.tokenize(text)
+            database.write(str(a_tag['href']) + '\t' + str(tokens))
+
+            for token in tokens:
+                if dictionary.get(token) is None:
+                    if token.istitle():
+                        continue
+                    else:
+                        print('new token!', token)
+                        database.write(str(token) + ', ')
+                        dictionary[token] = token
+                        # tweets stuff
+                        
+
+            database.write('\n')
+
+        except Exception as e:
+            print(e)
+
+database.close()
+
+output = open("./dictionary.json", 'w')
+json.dump(dictionary, output)
+output.close()
---
+++ readme.md
@@ -0,0 +1,13 @@
+
+Requisites
+----------
+
+Install PIP requirements with `pip install -r requirements.txt`.
+
+
+Start
+-----
+
+To start from scratch, download ilPost (or another newspaper website) with wget: `wget` into the scraped directory, then lauch `dataset_proc.py` to create the dictonary. 
+
+Run `html_proc.py` to launch the script.
