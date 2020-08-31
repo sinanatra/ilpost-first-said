22245e39fadcf8e529079317e028b8f2fe6808c1
Giacomo Nanni <gn.nanni@gmail.com>
2020-08-31 15:42:35 +0200

    feat: second bot

--- utils/tweet.py
+++ utils/tweet.py
@@ -5,12 +5,18 @@ import os
 auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET'])
 auth.set_access_token(os.environ['TWITTER_TOKEN'], os.environ['TWITTER_TOKEN_SECRET'])
 
+# Authenticate to Twitter, from the second account
+auth1 = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY1'], os.environ['TWITTER_API_SECRET1'])
+auth1.set_access_token(os.environ['TWITTER_TOKEN1'], os.environ['TWITTER_TOKEN_SECRET1'])
+
 # Create API object
 api = tweepy.API(auth)
+api1 = tweepy.API(auth1)
 
 statusid = ''
 
 def updateStatus (status, link):
     tweet = api.update_status(status)
     statusid = tweet.id_str
-    tweet = api.update_status(status +' appare in: @ilpostdice ' + link, statusid)
+    # reply to tweet
+    api1.update_status('@ilpostdice ' + status +' appare in:  ' + link, statusid)
