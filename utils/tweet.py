6f6284fa79bc8da7594dd08df91f85ea5d07c5b0
Giacomo Nanni <gn.nanni@gmail.com>
2020-09-02 22:22:40 +0200

    Update tweet.py

--- utils/tweet.py
+++ utils/tweet.py
@@ -5,8 +5,12 @@ import os
 auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET'])
 auth.set_access_token(os.environ['TWITTER_TOKEN'], os.environ['TWITTER_TOKEN_SECRET'])
 
+auth1 = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY1'], os.environ['TWITTER_API_SECRET1'])
+auth1.set_access_token(os.environ['TWITTER_TOKEN1'], os.environ['TWITTER_TOKEN_SECRET1'])
+
 # Create API object
 api = tweepy.API(auth)
+api1 = tweepy.API(auth1)
 
 statusid = ''
 
