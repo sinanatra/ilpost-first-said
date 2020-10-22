f0cf0d65282ec1b818efca7e03c0d6d1e8a19068
Giacomo Nanni <gn.nanni@gmail.com>
2020-10-22 09:40:42 +0200

    Update tweet.py

--- utils/tweet.py
+++ utils/tweet.py
@@ -5,11 +5,12 @@ import os
 auth = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY'], os.environ['TWITTER_API_SECRET'])
 auth.set_access_token(os.environ['TWITTER_TOKEN'], os.environ['TWITTER_TOKEN_SECRET'])
 
-#auth1 = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY1'], os.environ['TWITTER_API_SECRET1'])
-#auth1.set_access_token(os.environ['TWITTER_TOKEN1'], os.environ['TWITTER_TOKEN_SECRET1'])
+auth1 = tweepy.OAuthHandler(os.environ['TWITTER_API_KEY1'], os.environ['TWITTER_API_SECRET1'])
+auth1.set_access_token(os.environ['TWITTER_TOKEN1'], os.environ['TWITTER_TOKEN_SECRET1'])
 
 # Create API object
 api = tweepy.API(auth)
+api1 = tweepy.API(auth1)
 
 statusid = ''
 
@@ -18,7 +19,7 @@ def updateStatus (status, link):
     statusid = tweet.id_str
     # reply to tweet
     time.sleep(10)
-    #api.update_status('@ilpostdice ' + status +' appare in:  ' + link, statusid)
+    api1.update_status('@ilpostdice ' + status +' appare in:  ' + link, statusid)
 
 
 
