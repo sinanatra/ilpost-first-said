96eb275243d3f49cc406cf23f151442a15b4f3a5
Giacomo Nanni <gn.nanni@gmail.com>
2023-12-14 10:34:20 +0100

    fix: query properly the date field for the past 30 days

--- src/routes/api/get/+server.js
+++ src/routes/api/get/+server.js
@@ -1,38 +1,25 @@
-import db from "$lib/db"
-import { ObjectId } from 'mongodb';
+import db from "$lib/db";
 
 const now = new Date();
-const romeTimeString = now.toLocaleString('en-US', { timeZone:  'Europe/Rome' });
+const romeTimeString = now.toLocaleString('en-US', { timeZone: 'Europe/Rome' });
 const romeTime = new Date(romeTimeString);
 
 const start = new Date(romeTime - 30 * 24 * 60 * 60 * 1000); // 30 days ago
 const end = romeTime;
 
 export const GET = async ({ request, url }) => {
-
     const res = await db.collection('words').find({
-        _id: {
-            $gte: new ObjectId(Math.floor(start.getTime() / 1000).toString(16) + "0000000000000000"),
-            $lt: new ObjectId(Math.floor(end.getTime() / 1000).toString(16) + "0000000000000000")
+        date_added: {
+            $exists: true,
+            $gte: start.toISOString(),
+            $lt: end.toISOString()
         },
         word: {
             $regex: /\w{4,}/ // Match words with length greater than 3
         },
         url: { $exists: true },
-        date_added: { $exists: true },
-        context: { $exists: true } // Filter for documents where the "context" field exists
-    }).sort({ _id: -1 }).toArray();
-    
-
-
-
-    // REMOVES STUFFFF
-    // await db.collection('words').find({
-    //     _id: {
-    //         $gte: new ObjectId(Math.floor(start.getTime() / 1000).toString(16) + "0000000000000000"),
-    //         $lt: new ObjectId(Math.floor(end.getTime() / 1000).toString(16) + "0000000000000000")
-    //     }
-    // }).sort({ _id: -1 }).limit(200).forEach(doc => db.collection('words').deleteOne({ _id: doc._id }))
+        context: { $exists: true }
+    }).sort({ date_added: -1 }).toArray();
 
 
     const data = await res.map(({ word, date_added, url, context, _id }) => ({ word, date_added, url, context, _id }));
